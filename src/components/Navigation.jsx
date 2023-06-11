a88f4e76d34bf97e99add51a6f085780dc71454d
Michele <michelegec@gmail.com>
2023-06-11 22:49:00 +0200

    fix text-white

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,26 +3,26 @@ const Navigation = () => {
     <nav className="flex items-center justify-between px-4 py-2 text-white bg-[#14213D]">
       <div className="flex items-center">
         {/* <img src="/logo.png" alt="Logo" className="w-10 h-10 mr-2" /> */}
-        <h1 className="text-xl font-semibold text-black">FlexTalks</h1>
+        <h1 className="text-xl font-semibold ">FlexTalks</h1>
       </div>
       <ul className="flex space-x-4">
         <li>
-          <a href="#home" className="text-black hover:text-gray-200">
+          <a href="#home" className=" hover:text-gray-200">
             Home
           </a>
         </li>
         <li>
-          <a href="#courses" className="text-black hover:text-gray-200">
+          <a href="#courses" className=" hover:text-gray-200">
             Courses
           </a>
         </li>
         <li>
-          <a href="#about" className="text-black hover:text-gray-200">
+          <a href="#about" className=" hover:text-gray-200">
             About
           </a>
         </li>
         <li>
-          <a href="#contact" className="text-black hover:text-gray-200">
+          <a href="#contact" className=" hover:text-gray-200">
             Contact
           </a>
         </li>
